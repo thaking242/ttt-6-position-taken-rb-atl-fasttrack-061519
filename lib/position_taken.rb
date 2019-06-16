9d0f44ef3391bf3ddb365cb9afa4204de11f8ff0
Charleese burns <charleeseburns1@yahoo.com>
2019-06-16 18:48:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(array,index)
-  if (array [index] == || array[index] == " " || array[index]== nil)
+  if (array [index] == "" || array[index] == " " || array[index]== nil)
     return false
   else 
    return true
